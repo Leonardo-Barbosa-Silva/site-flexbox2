624ffe8cffba49b97a49f5b12e172a84d24111a9
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2023-09-01 12:48:48 -0300

    preparing to deploy

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.static('./public'))
+const PORT = process.env.PORT || process.env.PORT || 6003
 
 
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running...`)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log(`Haircut website is running on port ${PORT}`)
 })
